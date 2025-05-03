5f35c19d375a961b9ea3ec7dab181dca56a3b9c0
karla <karlakiwoso07@gmail.com>
2025-05-03 15:20:06 +0200

    appels des fonction dans main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'dart:io';
-    void main(){
-afficher();
-    }
-void afficher(){
-  print("entrez un nombre : ");
-  int a = int.parse(stdin.readLineSync()!);
-  print("entre un autre nombre ");
-  int b = int.parse(stdin.readLineSync()!);
-  int somme = calculer(a, b);
-  calculer(a, b);
-  print(somme);
-}
-int  calculer(int a, int b){
-  int somme =  a+b;
- return somme;
+import 'Menu.dart';
+import 'operation.dart';
 
+void main(){
+   bool continuer = true;
+
+   while(continuer){
+     menu();
+     print("l'addition de 4 +2 = ${addition(4, 2)}");
+     print("la soustracition de 4 - 2 = ${soustraction(4, 2)}");
+     print("la multiplication  de 4 * 2 = ${mutiplication(4, 2)}");
+     print("la division de 4 / 2 = ${division(4, 2)}");
+
+     break;
+
+
+   }
 }
